cbdf321e12439c38c12aae19fc96a01905c6736a
hrieger <H.Rieger@netze-odr.de>
2026-05-19 10:39:27 +0200

    functionen

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 let x; 
 let y;
-const input = document.getElementById("number1");
-const clickbtn = document.getElementById("btn");
-const output = document.getElementById("text")
+// const input = document.getElementById("number1");
+// const clickbtn = document.getElementById("btn");
+// const output = document.getElementById("text")
+const zplus = document.getElementById("plus");
+const zminus = document.getElementById("minus");
+const zmal = document.getElementById("mal");
+const zteil = document.getElementById("teil");
+
+const Input = document.getElementById("Input");
+const Input1 = document.getElementById("Input1");
 
 
-function add (x, y){
-    return x+y
+function add (input, input1){
+    return input + input1 
 }
 
 // add functions for substracting multiplying and dividing
-function substracting (x, y){
-    return x-y
+function substracting (input, input1){
+    return input - input1 
 }
 
-function multiplying (x, y){
-    return x*y
+function multiplying (input, input1){
+    return input * input1 
 }
 
-function dividing (x, y){
-    return x/y
+function dividing (input, input1){
+    return input / input1 
 }
 
 
