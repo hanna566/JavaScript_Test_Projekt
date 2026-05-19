b5396e7618827abf99853143fcbd1acdc9cffd17
hrieger <H.Rieger@netze-odr.de>
2026-05-19 08:16:07 +0200

    Rechner

---
+++ script.js
@@ -0,0 +1,29 @@
+let x; 
+let y;
+const input = document.getElementById("number1");
+const clickbtn = document.getElementById("btn");
+const output = document.getElementById("text")
+
+
+function add (x, y){
+    return x+y
+}
+
+// add functions for substracting multiplying and dividing
+function substracting (x, y){
+    return x-y
+}
+
+function multiplying (x, y){
+    return x*y
+}
+
+function dividing (x, y){
+    return x/y
+}
+
+
+
+function ClickButton (){
+    output.innerHTML = "Hallo!"
+}
