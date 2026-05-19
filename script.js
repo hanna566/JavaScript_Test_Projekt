5b8c2d2b5705239981c7cccca175f999d18111ab
hrieger <H.Rieger@netze-odr.de>
2026-05-19 11:20:15 +0200

    output

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let y;
 // const input = document.getElementById("number1");
 // const clickbtn = document.getElementById("btn");
 // const output = document.getElementById("text")
+
+// Nummern
+const btn1 = document.getElementById("Btn1");
+const btn2 = document.getElementById("Btn2");
+const btn3 = document.getElementById("Btn3");
+const btn4 = document.getElementById("Btn4");
+const btn5 = document.getElementById("Btn5");
+const btn6 = document.getElementById("Btn6");
+const btn7 = document.getElementById("Btn7");
+const btn8 = document.getElementById("Btn8");
+const btn9 = document.getElementById("Btn9");
+
+// Rechenzeichen
 const zplus = document.getElementById("plus");
 const zminus = document.getElementById("minus");
 const zmal = document.getElementById("mal");
@@ -31,6 +44,47 @@ function dividing (input, input1){
 
 
 
-function ClickButton (){
-    output.innerHTML = "Hallo!"
+function ClickButton1 (){
+    
+    output.innerHTML = "1"
+}
+
+function ClickButton2 (){
+    
+    output.innerHTML = "2"
+}
+
+function ClickButton3 (){
+    
+    output.innerHTML = "3"
+}
+
+function ClickButton4 (){
+    
+    output.innerHTML = "4"
+}
+
+function ClickButton5 (){
+    
+    output.innerHTML = "5"
+}
+
+function ClickButton6 (){
+    
+    output.innerHTML = "6"
+}
+
+function ClickButton7 (){
+    
+    output.innerHTML = "7"
+}
+
+function ClickButton8 (){
+    
+    output.innerHTML = "8"
+}
+
+function ClickButton9 (){
+    
+    output.innerHTML = "9"
 }
